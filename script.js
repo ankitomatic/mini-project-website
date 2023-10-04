828323d776ee603e8e85645c3775be7eb54e04cc
Ankit <ankittemail@gmail.com>
2023-10-05 02:44:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+const wrapper = document.querySelector('.wrapper');
+const loginLink = document.querySelector('.login-link');
+const registerLink = document.querySelector('.register-link');
+const btnPopup = document.querySelector('.btnlogin-popup');
+const iconClose = document.querySelector('.icon-close');
+
+registerLink.addEventListener('click',()=>{
+    wrapper.classList.add('active');
+});
+
+loginLink.addEventListener('click',()=>{
+    wrapper.classList.remove('active');
+});
+
+btnPopup.addEventListener('click',()=>{
+    wrapper.classList.add('active-popup');
+});
+
+iconClose.addEventListener('click',()=>{
+    wrapper.classList.remove('active-popup')
+});
+
+gsap.from('#page1 h1',{
+    y:100,
+    opacity:0,
+    delay:0.9,
+    stagger:0.2
+})
